8662ee7a78250577656a8dbbd4b128bd6435891a
tocomboy <tocomboy@naver.com>
2024-09-03 20:01:40 +0900

    대학 공부

---
+++ 알고리즘(신찬수)/Python/Point_클래스_선언.py
@@ -0,0 +1,10 @@
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+        
+    def __str__(self): # magic method
+        return f"({self.x}, {self.y})" # f-string(f 문자열)
+    
+p = Point(1, 2)
+s = str(p
